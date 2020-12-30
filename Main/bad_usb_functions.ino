6cbc0cc23c9e2922f3168a1f8a8f8c205991cd9e
joscor34 <estevajose4@gmail.com>
2020-12-29 19:15:20 -0600

    Se logro poner las funciones para pasar codigo de duckyScript a arduino

--- Main/bad_usb_functions.ino
+++ Main/bad_usb_functions.ino
@@ -1,60 +1,213 @@
-// This codes is just a prove. It will change when components arrive.
-#include <Keyboard.h>
+#include <SPI.h>
+#include <SD.h>
+#include <string.h>
+#include "Keyboard.h"
 
-void ejecute() {
-  Keyboard.begin();
-  delay(500);;
-  script();
-  Keyboard.end();
+File myFile;
+boolean first = true;
+String DEFAULT_FILE_NAME = "rickroll.txt";
+
+void runProgram() {
+Serial.print("Initializing SD card...");
+  if (!SD.begin(10)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("initialization done.");
+  myFile = SD.open(DEFAULT_FILE_NAME);
+  String line = "";
+  if(myFile){
+    while (myFile.available()) {
+      char m = myFile.read();
+      Serial.write(m);
+      if (m == '\n'){
+        Line(line);
+        line = "";
+        }
+        else if((int) m != 13)
+        {
+          line += m;
+        }
+     
+    }
+  myFile.close();
+  }else {
+    Serial.println("error opening rickroll.txt");
+  }
+
 }
 
-void typeKey(int key)
+void Line(String l)
 {
-  Keyboard.press(key);
-  delay(50);
-  Keyboard.release(key);
+  int space_1 = l.indexOf(" ");
+  if (space_1 == -1)
+  {
+    Press(l);
+  }
+  else if (l.substring(0,space_1) == "STRING")
+  {
+    Keyboard.print(l.substring(space_1 + 1));
+  }
+  else if (l.substring(0,space_1) == "DELAY")
+  {
+    int delaytime = l.substring(space_1 + 1).toInt();
+    delay(delaytime);
+  }
+  else if(l.substring(0,space_1) == "REM"){}
+  else
+  {
+      String remain = l;
+
+      while(remain.length() > 0)
+      {
+        int latest_space = remain.indexOf(" ");
+        if (latest_space == -1)
+        {
+          Press(remain);
+          remain = "";
+        }
+        else
+        {
+          Press(remain.substring(0, latest_space));
+          remain = remain.substring(latest_space + 1);
+        }
+        delay(5);
+      }
+  }
+
+  Keyboard.releaseAll();
 }
 
-void script() {
-  Keyboard.press(KEY_LEFT_GUI);
-  Keyboard.press(114);
-  Keyboard.releaseAll();
 
-  delay(200);
-
-  Keyboard.print("notepad");
-
-  typeKey(KEY_RETURN);
-  delay(100);
-  Keyboard.print("YOU HAVE BEEN PWNED MY BRUTHA\n");
-  typeKey(KEY_RETURN);
-  Keyboard.print(" &&&&&&&                                     .#&&@&&@&\n");
-  Keyboard.print(" &(((&&&&&&&@@@@@@&@&&&&&&&&&@&&&&&@&&&&&&&@@@@@@&&(&\n");
-  Keyboard.print("  &((((&&&&&&&&&@@@@@&&@&&@&&@@@&@@@&@&&@@&&@@@@(((@*\n");
-  Keyboard.print("   &&((&&@&@@&&&@@&@@@@@@&&@@&@@&&@@@&@@@@@@@@((((&\n");
-  Keyboard.print("     &&&@&@&@@&&@&&@&@@@@@@@@@@&@@@@@@@@@@@@@@&@#&\n");
-  Keyboard.print("    #&&@&@@@&@&&@@&@&@@&@@@@@@@@@@@@@@&@@@@@&@@@@(\n");
-  Keyboard.print("   &&&&&@@&@@@&&@@@@@@&@@@@@@@@@@@@@@@@@@@@@@@@&@&,\n");
-  Keyboard.print("  &&&@&@@&@@#@@@@@@&@@@&@@@@@@@@@@@@@@@@@@@@@@@@@&\n");
-  Keyboard.print("  @&&@@@@@##  %@@&#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&\n");
-  Keyboard.print(" &&&&@@@@&@#@@@  ##&@@&@@@@@@@@@@@@@@@@@@@@@@@@@@@@&\n");
-  Keyboard.print(" &@&@@&@@@&@@@&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&\n");
-  Keyboard.print(" @@@@@@@@@@@&@&@@@@@@@@@((((@@@@@@@@@@@@@@@@@@@@@@@@%\n");
-  Keyboard.print(" @@@&@&@@@@@@@@@@@@@@@@@@(#@@@@@@@@@@@@@@@@@@@@@@@&\n");
-  Keyboard.print("   &@@@@&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&\n");
-  Keyboard.print("     &&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
-  Keyboard.print("        &&@@@@@@@@@@@@@@@@@@@@&@@@@@@@@@@@@@&#\n");
-  Keyboard.print("            /@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
-  Keyboard.print("               *&@@@@@@@///((@@@@@@@@@&*\n");
-  Keyboard.print("                         (((\n");
-  typeKey(KEY_RETURN);
-  typeKey(KEY_RETURN);
-  Keyboard.print("GRRRRRR...\n");
-  typeKey(KEY_RETURN);
-  Keyboard.print("GOOD BOY");
-  delay(3000);
-
-  // End Payload
-
-  // Stop Keyboard and Mouse
+void Press(String b)
+{
+  if(b.length() == 1)
+  {
+    char c = b[0];
+    Keyboard.press(c);
+  }
+  else if (b.equals("ENTER"))
+  {
+    Keyboard.press(KEY_RETURN);
+  }
+  else if (b.equals("CTRL"))
+  {
+    Keyboard.press(KEY_LEFT_CTRL);
+  }
+    else if (b.equals("SHIFT"))
+  {
+    Keyboard.press(KEY_LEFT_SHIFT);
+  }
+    else if (b.equals("ALT"))
+  {
+    Keyboard.press(KEY_LEFT_ALT);
+  }
+    else if (b.equals("GUI"))
+  {
+    Keyboard.press(KEY_LEFT_GUI);
+  }
+    else if (b.equals("UP") || b.equals("UPARROW"))
+  {
+    Keyboard.press(KEY_UP_ARROW);
+  }
+    else if (b.equals("DOWN") || b.equals("DOWNARROW"))
+  {
+    Keyboard.press(KEY_DOWN_ARROW);
+  }
+    else if (b.equals("LEFT") || b.equals("LEFTARROW"))
+  {
+    Keyboard.press(KEY_LEFT_ARROW);
+  }
+    else if (b.equals("RIGHT") || b.equals("RIGHTARROW"))
+  {
+    Keyboard.press(KEY_RIGHT_ARROW);
+  }
+    else if (b.equals("DELETE"))
+  {
+    Keyboard.press(KEY_DELETE);
+  }
+    else if (b.equals("PAGEUP"))
+  {
+    Keyboard.press(KEY_PAGE_UP);
+  }
+    else if (b.equals("PAGEDOWN"))
+  {
+    Keyboard.press(KEY_PAGE_DOWN);
+  }
+    else if (b.equals("HOME"))
+  {
+    Keyboard.press(KEY_HOME);
+  }
+    else if (b.equals("ESC"))
+  {
+    Keyboard.press(KEY_ESC);
+  }
+    else if (b.equals("INSERT"))
+  {
+    Keyboard.press(KEY_INSERT);
+  }
+    else if (b.equals("TAB"))
+  {
+    Keyboard.press(KEY_TAB);
+  }
+    else if (b.equals("END"))
+  {
+    Keyboard.press(KEY_END);
+  }
+    else if (b.equals("CAPSLOCK"))
+  {
+    Keyboard.press(KEY_CAPS_LOCK);
+  }
+    else if (b.equals("F1"))
+  {
+    Keyboard.press(KEY_F1);
+  }
+    else if (b.equals("F2"))
+  {
+    Keyboard.press(KEY_F2);
+  }
+    else if (b.equals("F3"))
+  {
+    Keyboard.press(KEY_F3);
+  }
+    else if (b.equals("F4"))
+  {
+    Keyboard.press(KEY_F4);
+  }
+    else if (b.equals("F5"))
+  {
+    Keyboard.press(KEY_F5);
+  }
+    else if (b.equals("F6"))
+  {
+    Keyboard.press(KEY_F6);
+  }
+    else if (b.equals("F7"))
+  {
+    Keyboard.press(KEY_F7);
+  }
+    else if (b.equals("F8"))
+  {
+    Keyboard.press(KEY_F8);
+  }
+    else if (b.equals("F9"))
+  {
+    Keyboard.press(KEY_F9);
+  }
+    else if (b.equals("F10"))
+  {
+    Keyboard.press(KEY_F10);
+  }
+    else if (b.equals("F11"))
+  {
+    Keyboard.press(KEY_F11);
+  }
+    else if (b.equals("F12"))
+  {
+    Keyboard.press(KEY_F12);
+  }
+    else if (b.equals("SPACE"))
+  {
+    Keyboard.press(' ');
+  } 
 }
